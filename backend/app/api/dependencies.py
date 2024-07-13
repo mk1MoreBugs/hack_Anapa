d94525f62b5497c33f855a37aa0b0f98e1359722
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-07-13 14:10:56 +0300

    add dependencies.py and main.py for api module

---
+++ backend/app/api/dependencies.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from app.database.database import create_db_engine
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
+
+
+engine = create_db_engine(
+    database_url=SQLALCHEMY_DATABASE_URL,
+)
+
+
+def session_db():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(session_db)]
--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
+from app.api.routes import platform
+
 api_router = APIRouter()
+
+api_router.include_router(platform.router)
