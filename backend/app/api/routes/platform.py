cef4064fc449404520aee4388aa9708db4f40c93
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-07-13 14:11:40 +0300

    implement platform routes

---
+++ backend/app/api/routes/platform.py
@@ -0,0 +1,47 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Query
+
+from app.api.dependencies import SessionDep
+from app.database.crud.platform import read_all_platform, read_platform_by_address, \
+    read_platform_price_up_and_price_down, create_platform
+from app.database.models.platform import Platform
+
+router = APIRouter(
+    prefix="/platforms",
+    tags=["platform routes"],
+)
+
+
+@router.get("/")
+def get_platforms(
+        session: SessionDep,
+        address: Annotated[str | None, Query()] = None,
+) -> list[Platform]:
+    if address:
+        list_platforms: list[Platform] = read_platform_by_address(
+            session=session,
+            address=address
+        )
+    else:
+        list_platforms: list[Platform] = read_all_platform(session)
+    return list_platforms
+
+
+@router.get("/price")
+def get_platforms(
+        session: SessionDep,
+        price_up: Annotated[int, Query()],
+        price_down: Annotated[int, Query()],
+) -> list[Platform]:
+    list_platforms: list[Platform] = read_platform_price_up_and_price_down(
+        session=session,
+        price_up=price_up,
+        price_down=price_down,
+    )
+    return list_platforms
+
+
+@router.post("/")
+def create_new_platform(session: SessionDep, platform: Platform):
+    create_platform(session=session, platform=platform)
